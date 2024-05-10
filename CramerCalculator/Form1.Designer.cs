b834ac732ca571a718fad42576490a980ba1c006
KUD6SN1K <egor.saqds@gmail.com>
2024-05-10 17:15:41 +0300

    add basic form

--- CramerCalculator/Form1.Designer.cs
+++ CramerCalculator/Form1.Designer.cs
@@ -28,12 +28,361 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            textBox9 = new TextBox();
+            textBox10 = new TextBox();
+            textBox11 = new TextBox();
+            textBox12 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            button1 = new Button();
+            label15 = new Label();
+            label16 = new Label();
+            label17 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Light", 150F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label1.Location = new Point(195, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 265);
+            label1.TabIndex = 0;
+            label1.Text = "{";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(293, 146);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(31, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(372, 146);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(31, 23);
+            textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(454, 145);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(31, 23);
+            textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(542, 146);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(31, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(293, 193);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(31, 23);
+            textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(372, 193);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(31, 23);
+            textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(454, 193);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(31, 23);
+            textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(542, 193);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(31, 23);
+            textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            textBox9.Location = new Point(293, 244);
+            textBox9.Name = "textBox9";
+            textBox9.Size = new Size(31, 23);
+            textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            textBox10.Location = new Point(372, 244);
+            textBox10.Name = "textBox10";
+            textBox10.Size = new Size(31, 23);
+            textBox10.TabIndex = 10;
+            // 
+            // textBox11
+            // 
+            textBox11.Location = new Point(454, 243);
+            textBox11.Name = "textBox11";
+            textBox11.Size = new Size(31, 23);
+            textBox11.TabIndex = 11;
+            // 
+            // textBox12
+            // 
+            textBox12.Location = new Point(542, 243);
+            textBox12.Name = "textBox12";
+            textBox12.Size = new Size(31, 23);
+            textBox12.TabIndex = 12;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Times New Roman", 14F);
+            label2.Location = new Point(326, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(46, 22);
+            label2.TabIndex = 13;
+            label2.Text = "x1 +";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Times New Roman", 14F);
+            label3.Location = new Point(407, 146);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 22);
+            label3.TabIndex = 14;
+            label3.Text = "x2 +";
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Times New Roman", 14F);
+            label4.Location = new Point(488, 146);
+            label4.Name = "label4";
+            label4.Size = new Size(46, 22);
+            label4.TabIndex = 15;
+            label4.Text = "x3";
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Times New Roman", 16F);
+            label5.Location = new Point(515, 143);
+            label5.Name = "label5";
+            label5.Size = new Size(27, 22);
+            label5.TabIndex = 16;
+            label5.Text = "=";
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Times New Roman", 14F);
+            label6.Location = new Point(326, 193);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 22);
+            label6.TabIndex = 17;
+            label6.Text = "x1 +";
+            // 
+            // label7
+            // 
+            label7.Font = new Font("Times New Roman", 14F);
+            label7.Location = new Point(407, 193);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 22);
+            label7.TabIndex = 18;
+            label7.Text = "x2 +";
+            // 
+            // label8
+            // 
+            label8.Font = new Font("Times New Roman", 14F);
+            label8.Location = new Point(488, 193);
+            label8.Name = "label8";
+            label8.Size = new Size(46, 22);
+            label8.TabIndex = 19;
+            label8.Text = "x3";
+            // 
+            // label9
+            // 
+            label9.Font = new Font("Times New Roman", 16F);
+            label9.Location = new Point(515, 191);
+            label9.Name = "label9";
+            label9.Size = new Size(27, 22);
+            label9.TabIndex = 20;
+            label9.Text = "=";
+            // 
+            // label10
+            // 
+            label10.Font = new Font("Times New Roman", 14F);
+            label10.Location = new Point(326, 244);
+            label10.Name = "label10";
+            label10.Size = new Size(46, 22);
+            label10.TabIndex = 21;
+            label10.Text = "x1 +";
+            // 
+            // label11
+            // 
+            label11.Font = new Font("Times New Roman", 14F);
+            label11.Location = new Point(407, 244);
+            label11.Name = "label11";
+            label11.Size = new Size(46, 22);
+            label11.TabIndex = 22;
+            label11.Text = "x2 +";
+            // 
+            // label12
+            // 
+            label12.Font = new Font("Times New Roman", 14F);
+            label12.Location = new Point(488, 244);
+            label12.Name = "label12";
+            label12.Size = new Size(46, 22);
+            label12.TabIndex = 23;
+            label12.Text = "x3";
+            // 
+            // label13
+            // 
+            label13.Font = new Font("Times New Roman", 16F);
+            label13.Location = new Point(515, 242);
+            label13.Name = "label13";
+            label13.Size = new Size(27, 22);
+            label13.TabIndex = 24;
+            label13.Text = "=";
+            // 
+            // label14
+            // 
+            label14.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            label14.Location = new Point(195, 58);
+            label14.Name = "label14";
+            label14.Size = new Size(547, 22);
+            label14.TabIndex = 25;
+            label14.Text = "Решить систему линейных уравнений методом Крамера";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Times New Roman", 14F);
+            button1.Location = new Point(293, 337);
+            button1.Name = "button1";
+            button1.Size = new Size(280, 42);
+            button1.TabIndex = 26;
+            button1.Text = "Решить систему уравнений";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label15
+            // 
+            label15.Font = new Font("Times New Roman", 14F);
+            label15.Location = new Point(213, 419);
+            label15.Name = "label15";
+            label15.Size = new Size(46, 22);
+            label15.TabIndex = 27;
+            label15.Text = "x1=";
+            // 
+            // label16
+            // 
+            label16.Font = new Font("Times New Roman", 14F);
+            label16.Location = new Point(372, 419);
+            label16.Name = "label16";
+            label16.Size = new Size(46, 22);
+            label16.TabIndex = 28;
+            label16.Text = "x2=";
+            // 
+            // label17
+            // 
+            label17.Font = new Font("Times New Roman", 14F);
+            label17.Location = new Point(527, 419);
+            label17.Name = "label17";
+            label17.Size = new Size(46, 22);
+            label17.TabIndex = 29;
+            label17.Text = "x3=";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(814, 472);
+            Controls.Add(label17);
+            Controls.Add(label16);
+            Controls.Add(label15);
+            Controls.Add(button1);
+            Controls.Add(label14);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox12);
+            Controls.Add(textBox11);
+            Controls.Add(textBox10);
+            Controls.Add(textBox9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Times New Roman", 10F);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CramerCalculator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private TextBox textBox9;
+        private TextBox textBox10;
+        private TextBox textBox11;
+        private TextBox textBox12;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private Button button1;
+        private Label label15;
+        private Label label16;
+        private Label label17;
     }
 }
